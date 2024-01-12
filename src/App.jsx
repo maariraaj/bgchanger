317cff9902029fe242206c26c46946e0a8147545
maariraaj <maari000@hotmail.com>
2024-01-12 13:16:44 +0530

    bgc

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive');
+
+  // function changeColor(color){
+  //   setColor(color)
+  // }
+
+  return (
+    <>
+    <div className="w-full h-screen duration-200" 
+    style={{backgroundColor:color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          {/* <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black' onClick={()=> changeColor('red')}>Red</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black'onClick={()=> changeColor('green')}>Green</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black'onClick={()=> changeColor('blue')}>Blue</button> */}
+
+
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black' onClick={()=> setColor('red')} style={{backgroundColor: 'red'}}>Red</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black'onClick={()=> setColor('green')} style={{backgroundColor: 'green'}}>Green</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-black'onClick={()=> setColor('blue')} style={{backgroundColor: 'blue'}}>Blue</button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
